83349cfa3d52dd62f621ffe9a00fd2ff92d09c13
Prini_Tiyu_10851859 <prini.tiyu@ltm.com>
2026-04-13 14:15:35 +0530

    Delete Prini Tiyu_10851859/utils directory

--- Prini Tiyu_10851859/utils/ocr_utils.py
+++
@@ -1,9 +0,0 @@
-import io
-from PIL import Image
-import pytesseract
-
-def ocr_image_bytes(image_bytes: bytes) -> str:
-    img = Image.open(io.BytesIO(image_bytes))
-    # You can optionally do img = img.convert("L") for better OCR sometimes
-    text = pytesseract.image_to_string(img)
-    return text or ""
